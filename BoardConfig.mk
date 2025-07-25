934d132f4128c9d7ac97759f4e9265866bb19fb3
dianlujitao <dianlujitao@lineageos.org>
2025-07-25 20:15:59 +0800

    peridot: Set force_sysfs_fallback=1 sysctl via cmdline
    early-init class gets triggered after all recovery modules are already
    loaded, which prevents ADSP firmware from being loaded.
    
    Change-Id: I4eaf2890bd98a6a11b6a87ad7330827fbf9b44d7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,6 +111,7 @@ TARGET_KERNEL_CONFIG := \
 
 BOARD_KERNEL_CMDLINE := \
     video=vfb:640x400,bpp=32,memsize=3072000 \
+    sysctl.kernel.firmware_config.force_sysfs_fallback=1 \
     swinfo.fingerprint=peridot:$(LINEAGE_VERSION) \
     mtdoops.fingerprint=peridot:$(LINEAGE_VERSION)
 
