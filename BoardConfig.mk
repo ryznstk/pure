9eab7034125cc68ae2b42690e52af236827efbc9
AdarshGrewal <adarshgrewal@gmail.com>
2026-05-02 20:25:51 +0530

    peridot: Update from OS3.0.6.0.WNPMIXM
    Change-Id: I2640ee37d6f0c43b4568f5e4f724d8b04177006a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -235,7 +235,7 @@ include $(DEVICE_PATH)/sepolicy/SEPolicy-diag.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Vendor security patch
-VENDOR_SECURITY_PATCH := 2025-12-01
+VENDOR_SECURITY_PATCH := 2026-02-01
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
