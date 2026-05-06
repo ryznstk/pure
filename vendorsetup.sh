2083e31a5d46c74ccc7c02ec080776a7f3f5c0de
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-05-06 09:32:35 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -41,6 +41,11 @@ git fetch https://github.com/ryznstk/lunaris_system_sepolicy.git test
 git reset --hard FETCH_HEAD
 croot
 
+cd packages/apps/Settings
+git fetch https://github.com/ryznstk/packages_apps_Settings test
+git reset --hard FETCH_HEAD
+croot
+
 # Refresh signing keys
 if [ -d vendor/lineage-priv/keys ]; then
   echo "Removing existing signing keys..."
