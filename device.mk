d5a05d1638707d5b39cb59c04032ac7dcd145406
ryznstk <agusmhrd1k4@gmail.com>
2026-05-18 18:54:15 +0700

    Enable UFFD GC

--- device.mk
+++ device.mk
@@ -41,6 +41,8 @@ $(call inherit-product-if-exists, device/xiaomi/peridot-miuicamera/device.mk)
 # Viper4AndroidFX
 $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 
+PRODUCT_ENABLE_UFFD_GC := true
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
