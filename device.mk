e09ec36182c257b0287e7a5069405b68dafd452e
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-05-21 06:48:10 +0700

    Add AxionFx configuration to device.mk

--- device.mk
+++ device.mk
@@ -29,6 +29,8 @@ $(call soong_config_set,surfaceflinger,frame_rate_category_min,60)
 # TouchServices
 $(call inherit-product, packages/apps/TouchServices/touchservice.mk)
 
+$(call inherit-product, packages/apps/AxionFx/config.mk)
+
 # GameBar Performance Overlay
 $(call inherit-product, packages/apps/GameBar/gamebar.mk) 
 
@@ -376,7 +378,8 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi \
     vendor/qcom/opensource/usb/etc \
-    hardware/lineage/compat
+    hardware/lineage/compat \
+    packages/apps/AxionFx
 
 # Sensors
 PRODUCT_PACKAGES += \
