717840d989c92d66283d8391744ac6ae118273b9
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-06-02 10:28:18 +0700

    Modify vendor and kernel source cloning in vendorsetup.sh
    Updated git clone commands to use specific branches and changed some repository URLs.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,18 +3,18 @@
 # Vendor (fresh clone)
 echo "Cloning vendor tree..."
 rm -rf vendor/xiaomi/peridot
-git clone https://gitlab.com/blu96/vendor-xiaomi-peridot-yt.git vendor/xiaomi/peridot
+git clone -b los https://gitlab.com/blu96/vendor-xiaomi-peridot-yt.git vendor/xiaomi/peridot
 
 # Kernel source (fresh clone)
 echo "Cloning kernel source tree..."
 rm -rf kernel/xiaomi/sm8635
-git clone -b los --depth 1 https://gitlab.com/blu96/kernel_xiaomi_sm8635_peridot.git kernel/xiaomi/sm8635
+git clone -b test6 --depth 1 https://gitlab.com/blu96/kernel_xiaomi_sm8635_peridot.git kernel/xiaomi/sm8635
 
 rm -rf kernel/xiaomi/sm8635-modules
-git clone -b lineage-23.2 --depth 1 https://github.com/LineageOS/android_kernel_xiaomi_sm8635-modules.git kernel/xiaomi/sm8635-modules
+git clone -b lineage-23.2 --depth 1 https://github.com/ryznstk/kernel_xiaomi_sm8635-modules.git kernel/xiaomi/sm8635-modules
 
 rm -rf kernel/xiaomi/sm8635-devicetrees
-git clone -b lineage-23.2 --depth 1 https://github.com/LineageOS/android_kernel_xiaomi_sm8635-devicetrees.git kernel/xiaomi/sm8635-devicetrees
+git clone -b lineage-23.2 --depth 1 https://github.com/ryznstk/kernel_xiaomi_sm8635-devicetrees.git kernel/xiaomi/sm8635-devicetrees
 
 # Hardware xiaomi (fresh clone)
 echo "Cloning hardware xiaomi source..."
@@ -54,10 +54,6 @@ rm -rf packages/apps/XiaomiParts
 rm -rf packages/apps/ViPER4AndroidFX
 git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX
 
-
-rm -rf packages/apps/TouchServices
-git clone https://github.com/kenway214/packages_apps_TouchServices.git -b lineage-23.2 packages/apps/TouchServices
-
 rm -rf vendor/evolution-priv
 
 # Refresh signing keys
@@ -72,7 +68,7 @@ git clone https://github.com/droidcore/private_key.git -b main vendor/lineage-pr
 if command -v croot &>/dev/null; then
   croot
 else
-  cd "$ANDROID_BUILD_TOP" || true
+  cd "$ANDRID_BUILD_TOP" || true
 fi
 
 echo "vendorsetup.sh execution complete."
