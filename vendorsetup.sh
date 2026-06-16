449a7448c3a40ae82f0d5672a93d30b1f078bf03
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-06-16 15:49:46 +0700

    Change kernel source branch from 'los' to 'sukisu'

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,7 +8,7 @@ git clone -b los https://gitlab.com/blu96/vendor-xiaomi-peridot-yt.git vendor/xi
 # Kernel source (fresh clone)
 echo "Cloning kernel source tree..."
 rm -rf kernel/xiaomi/sm8635
-git clone -b los --depth 1 https://gitlab.com/blu96/xiaomi_sm8635.git kernel/xiaomi/sm8635
+git clone -b sukisu --depth 1 https://gitlab.com/blu96/xiaomi_sm8635.git kernel/xiaomi/sm8635
 
 rm -rf kernel/xiaomi/sm8635-modules
 git clone -b lineage-23.2 --depth 1 https://github.com/ryznstk/kernel_xiaomi_sm8635-modules.git kernel/xiaomi/sm8635-modules
