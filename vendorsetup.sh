9b0c0e5747a95ca1fc94657d202e7a8a1c7fa70c
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-06-19 08:19:19 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,13 +8,17 @@ git clone https://gitlab.com/blu96/vendor-xiaomi-peridot-yt.git vendor/xiaomi/pe
 # Kernel source (fresh clone)
 echo "Cloning kernel source tree..."
 rm -rf kernel/xiaomi/sm8635
-git clone -b los --depth 1 https://gitlab.com/blu96/xiaomi_sm8635.git kernel/xiaomi/sm8635
+git clone -b resuki --depth 1 https://gitlab.com/blu96/xiaomi_sm8635.git kernel/xiaomi/sm8635
 
 rm -rf kernel/xiaomi/sm8635-modules
 git clone -b lineage-23.2 --depth 1 https://github.com/ryznstk/kernel_xiaomi_sm8635-modules.git kernel/xiaomi/sm8635-modules
 
 rm -rf kernel/xiaomi/sm8635-devicetrees
 git clone -b lineage-23.2 --depth 1 https://github.com/ryznstk/kernel_xiaomi_sm8635-devicetrees.git kernel/xiaomi/sm8635-devicetrees
+
+cd kernel/xiaomi/sm8635
+curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/main/kernel/setup.sh" | bash
+croot
 
 # Hardware xiaomi (fresh clone)
 echo "Cloning hardware xiaomi source..."
@@ -63,7 +67,7 @@ if [ -d vendor/evolution-priv/keys ]; then
   rm -rf vendor/evolution-priv/keys
 fi
 echo "Cloning fresh signing keys..."
-git clone https://github.com/droidcore/private_key.git -b evo vendor/evolution-priv/keys
+git clone https://github.com/droidcore/private_key.git -b main vendor/evolution-priv/keys
 
 # Always back to root at the end
 if command -v croot &>/dev/null; then
