ab9a54cb3b0f0b9b655cd1e5a4ea1b54e68a9daf
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-06-26 19:55:20 +0700

    Modify blob_fixup for wfdmmsrc and wfdservice
    Updated dependencies for libwfdmmsrc_system.so and libwfdservice.so to include libaudiobase.so.

--- extract-files.py
+++ extract-files.py
@@ -97,11 +97,11 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace(r'\$\{ro\.boot\.vendor\.qspa:-default\}', 'default'),
     'system_ext/etc/vintf/manifest/vendor.qti.qesdsys.service.xml': blob_fixup()
         .regex_replace(r'(?s)^.*?(?=<manifest)', ''),
-    'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
-        .add_needed('libgui_shim.so'),
         'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
+        .add_needed('libaudiobase.so')
         .add_needed('libgui_shim.so'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .add_needed('libaudiobase.so')
         .replace_needed('android.media.audio.common.types-V4-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .add_needed('libbinder_shim.so')
