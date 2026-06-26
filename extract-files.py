70321ef560f8920252e1694ee18a5d081f974ddb
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-06-26 14:16:58 +0700

    Fix blob dependencies in extract-files.py

--- extract-files.py
+++ extract-files.py
@@ -99,6 +99,10 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace(r'(?s)^.*?(?=<manifest)', ''),
     'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
         .add_needed('libgui_shim.so'),
+        'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
+        .add_needed('libgui_shim.so'),
+    'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .replace_needed('android.media.audio.common.types-V4-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .add_needed('libbinder_shim.so')
         .add_needed('libinput_shim.so')
