0a55ec5882b28280b0418860c69d3c48c95d2622
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-06-26 14:04:43 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -41,6 +41,9 @@ $(call inherit-product-if-exists, device/xiaomi/peridot-miuicamera/device.mk)
 # Viper4AndroidFX
 $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.adb.secure=0
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
