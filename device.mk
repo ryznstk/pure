f0a41541728688e96acabfe1dbea99a2e8fe6ca9
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-06-26 08:21:40 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -376,8 +376,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
-    hardware/google/pixel/pixelstats \
-    hardware/google/pixel/power-libperfmgr \
+    hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi \
