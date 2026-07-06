977a2b7d7ff5f5f429e95006f75d65da686435c3
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-07-07 06:05:57 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,7 +8,7 @@ git clone -b lineage-23.2 https://gitlab.com/blu96/vendor-xiaomi-peridot-yt.git 
 # Kernel source (fresh clone)
 echo "Cloning kernel source tree..."
 rm -rf kernel/xiaomi/sm8635
-git clone -b resuki --depth 1 https://gitlab.com/blu96/xiaomi_sm8635.git kernel/xiaomi/sm8635
+git clone --depth 1 https://github.com/ryznstk/android_kernel_xiaomi_sm8635.git kernel/xiaomi/sm8635
 
 rm -rf kernel/xiaomi/sm8635-modules
 git clone -b lineage-23.2 --depth 1 https://github.com/LineageOS/android_kernel_xiaomi_sm8635-modules.git kernel/xiaomi/sm8635-modules
@@ -46,11 +46,6 @@ rm -rf packages/apps/XiaomiParts
 # ViperFX
 rm -rf packages/apps/ViPER4AndroidFX
 git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX
-
-cd system/sepolicy
-git fetch https://github.com/ryznstk/inf_system_sepolicy 16
-git reset --hard FETCH_HEAD
-croot
 
 rm -rf packages/apps/TouchServices
 git clone https://github.com/kenway214/packages_apps_TouchServices.git -b lineage-23.2 packages/apps/TouchServices
