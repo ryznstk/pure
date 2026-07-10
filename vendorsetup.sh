1aa5505b6115da74f23340a150d619fe11b527d4
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-07-10 07:39:02 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -54,6 +54,14 @@ git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages
 rm -rf packages/apps/TouchServices
 git clone https://github.com/ryznstk/packages_apps_TouchServices.git -b lineage-23.2 packages/apps/TouchServices
 
+# Refresh signing keys
+if [ -d vendor/evolution-priv/keys ]; then
+  echo "Removing existing signing keys..."
+  rm -rf vendor/evolution-priv/keys
+fi
+echo "Cloningfresh signing keys..."
+git clone https://github.com/ryznstk/keys.git vendor/evolution-priv/keys
+
 # Always back to root at the end
 if command -v croot &>/dev/null; then
   croot
