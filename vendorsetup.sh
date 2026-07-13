518dafc968dddd5b7721c560e2268b49ca2dbb84
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-07-14 05:47:33 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -47,18 +47,18 @@ git clone https://github.com/ryznstk/packages_apps_KProfiles.git packages/apps/K
 
 rm -rf packages/apps/XiaomiParts
 
-cd frameworks/base
-git fetch https://github.com/Lunaris-AOSP/frameworks_base test
-git reset --hard FETCH_HEAD
-croot
+#cd frameworks/base
+#git fetch https://github.com/Lunaris-AOSP/frameworks_base test
+#git reset --hard FETCH_HEAD
+#croot
 
-cd packages/apps/Singularity
-git fetch https://github.com/ryznstk/packages_apps_Singularity test
-git reset --hard FETCH_HEAD
-croot
+#cd packages/apps/Singularity
+#git fetch https://github.com/ryznstk/packages_apps_Singularity test
+#git reset --hard FETCH_HEAD
+#croot
 
 cd packages/apps/Settings
-git fetch https://github.com/ryznstk/packages_apps_Settings test
+git fetch https://github.com/ryznstk/packages_apps_Settings 16.2
 git reset --hard FETCH_HEAD
 croot
 
