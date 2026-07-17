eb7053a3327622a3f784f2f952624ac586dcef9f
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-07-17 09:01:47 +0700

    Clean up themes directory before cloning TouchServices
    Remove existing themes directory before cloning TouchServices.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -54,6 +54,8 @@ git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages
 rm -rf packages/apps/TouchServices
 git clone https://github.com/ryznstk/packages_apps_TouchServices.git -b lineage-23.2 packages/apps/TouchServices
 
+rm -rf vendor/extras/themes
+
 # Refresh signing keys
 if [ -d vendor/evolution-priv/keys ]; then
   echo "Removing existing signing keys..."
