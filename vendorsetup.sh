99056e1097b89c76c1344de18691c54651882070
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-07-25 05:15:39 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -54,7 +54,7 @@ git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages
 rm -rf packages/apps/TouchServices
 git clone https://github.com/ryznstk/packages_apps_TouchServices.git -b lineage-23.2 packages/apps/TouchServices
 
-rm-rf vendor/lineage-priv
+rm -rf vendor/lineage-priv
 
 # Refresh signing keys
 if [ -d vendor/evolution-priv/keys ]; then
