057fe3800d51e75a5adf31d8d7debf2dc768bcc5
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-07-25 05:19:53 +0700

    Update vendor setup to include new repositories
    Clone TouchServices and update vendor products.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -54,6 +54,11 @@ git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages
 rm -rf packages/apps/TouchServices
 git clone https://github.com/ryznstk/packages_apps_TouchServices.git -b lineage-23.2 packages/apps/TouchServices
 
+cd vendor/products
+git fetch https://github.com/ryznstk/platform_vendor_products 17
+git reset --hard FETCH_HEAD
+croot
+
 rm -rf vendor/lineage-priv
 
 # Refresh signing keys
