e36472ea120d8f4496f25bf30204812f8ce35634
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-07-29 04:47:57 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,8 +7,8 @@ git clone -b a17new https://gitlab.com/blu96/vendor-xiaomi-peridot-yt.git vendor
 
 # Kernel source (fresh clone)
 echo "Cloning kernel source tree..."
-rm -rf kernel/xiaomi/sm8635
-git clone -b resuki --depth 1 https://gitlab.com/blu96/xiaomi_sm8635.git kernel/xiaomi/sm8635
+#rm -rf kernel/xiaomi/sm8635
+#git clone -b resuki --depth 1 https://gitlab.com/blu96/xiaomi_sm8635.git kernel/xiaomi/sm8635
 
 rm -rf kernel/xiaomi/sm8635-modules
 git clone -b lineage-23.2 --depth 1 https://github.com/ryznstk/kernel_xiaomi_sm8635-modules.git kernel/xiaomi/sm8635-modules
