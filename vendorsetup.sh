347013d41dbd9e4d3ae3f3d74ca038681196b8f6
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-07-29 01:05:36 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,18 +7,18 @@ git clone -b a17new https://gitlab.com/blu96/vendor-xiaomi-peridot-yt.git vendor
 
 # Kernel source (fresh clone)
 echo "Cloning kernel source tree..."
-#rm -rf kernel/xiaomi/sm8635
-#git clone -b resuki --depth 1 https://gitlab.com/blu96/xiaomi_sm8635.git kernel/xiaomi/sm8635
+rm -rf kernel/xiaomi/sm8635
+git clone -b resuki --depth 1 https://gitlab.com/blu96/xiaomi_sm8635.git kernel/xiaomi/sm8635
 
-#rm -rf kernel/xiaomi/sm8635-modules
-#git clone -b lineage-23.2 --depth 1 https://github.com/ryznstk/kernel_xiaomi_sm8635-modules.git kernel/xiaomi/sm8635-modules
+rm -rf kernel/xiaomi/sm8635-modules
+git clone -b lineage-23.2 --depth 1 https://github.com/ryznstk/kernel_xiaomi_sm8635-modules.git kernel/xiaomi/sm8635-modules
 
-#rm -rf kernel/xiaomi/sm8635-devicetrees
-#git clone -b lineage-23.2 --depth 1 https://github.com/ryznstk/kernel_xiaomi_sm8635-devicetrees.git kernel/xiaomi/sm8635-devicetrees
+rm -rf kernel/xiaomi/sm8635-devicetrees
+git clone -b lineage-23.2 --depth 1 https://github.com/ryznstk/kernel_xiaomi_sm8635-devicetrees.git kernel/xiaomi/sm8635-devicetrees
 
-#cd kernel/xiaomi/sm8635
-#curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/main/kernel/setup.sh" | bash
-#croot
+cd kernel/xiaomi/sm8635
+curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/main/kernel/setup.sh" | bash
+croot
 
 # Hardware xiaomi (fresh clone)
 echo "Cloning hardware xiaomi source..."
