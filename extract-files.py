332a12181cb210bf67b76befa529753b6e77dade
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-08-01 06:01:48 +0700

    Remove blob_fixup for camera.xiaomi.so

--- extract-files.py
+++ extract-files.py
@@ -171,8 +171,6 @@ blob_fixups: blob_fixups_user_type = {
                 '_ZN5mihal9GraBufferC2EPKNS_6StreamEPK13native_handle',
             ],
     ),
-    'odm/lib64/hw/camera.xiaomi.so': blob_fixup()
-        .replace_needed('libui.so', 'libui-v34.so'),
     (
         'odm/lib64/camera/com.qti.actuator.peridot_aac_imx882_gt9764ber_wide_i_actuator.so',
         'odm/lib64/camera/com.qti.actuator.peridot_ofilm_imx882_aw86016csr_wide_ii_actuator.so',
