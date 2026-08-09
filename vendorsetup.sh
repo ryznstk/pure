d91a7779062bb16a848d3c50dc65b3e16f25120f
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-08-09 17:26:56 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,7 +11,7 @@ rm -rf kernel/xiaomi/sm8635
 git clone -b resuki --depth 1 https://gitlab.com/blu96/xiaomi_sm8635.git kernel/xiaomi/sm8635
 
 rm -rf kernel/xiaomi/sm8635-modules
-git clone -b ll --depth 1 https://github.com/ryznstk/kernel_xiaomi_sm8635-modules.git kernel/xiaomi/sm8635-modules
+git clone -b lineage-23.2 --depth 1 https://github.com/ryznstk/kernel_xiaomi_sm8635-modules.git kernel/xiaomi/sm8635-modules
 
 rm -rf kernel/xiaomi/sm8635-devicetrees
 git clone -b lineage-23.2 --depth 1 https://github.com/ryznstk/kernel_xiaomi_sm8635-devicetrees.git kernel/xiaomi/sm8635-devicetrees
@@ -76,34 +76,8 @@ croot
 rm -rf packages/apps/ViPER4AndroidFX
 git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX
 
-rm -rf packages/apps/AxionFx
-git clone https://github.com/AxionAOSP/android_packages_apps_AxionFx.git packages/apps/AxionFx
-
-rm -rf external/steam-audio
-git clone -b lineage-23.2 https://github.com/AxionAOSP/android_external_steam-audio.git external/steam-audio
-
 rm -rf packages/apps/TouchServices
 git clone https://github.com/kenway214/packages_apps_TouchServices.git -b lineage-23.2 packages/apps/TouchServices
-
-cd external/pffft
-git fetch https://github.com/AxionAOSP/android_external_pffft lineage-23.2
-git reset --hard FETCH_HEAD
-croot
-
-cd hardware/interfaces
-git fetch https://github.com/ryznstk/lunaris_hardware_interfaces 16.2
-git reset --hard FETCH_HEAD
-croot
-
-cd frameworks/av
-git fetch https://github.com/AxionAOSP/android_frameworks_av lineage-23.2
-git cherry-pick 463cdb4267ceece8c7b630cb0339ea26e985075c
-croot
-
-cd system/media
-git fetch https://github.com/ryznstk/system_media bq2
-git reset --hard FETCH_HEAD
-croot
 
 #cd packages/apps/Launcher3
 #git fetch https://github.com/Lunaris-AOSP/packages_apps_Launcher3 16.2
