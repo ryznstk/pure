ac0edfe8b3627d798fda4d0bd9b46650a96e3bca
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-08-17 09:21:10 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -76,11 +76,6 @@ git fetch https://github.com/ryznstk/frameworks_av 16.2
 git reset --hard FETCH_HEAD
 croot
 
-cd system/media
-git fetch https://github.com/AxionAOSP/android_system_media.git lineage-23.2
-git cherry-pick 2d58bc4b728d3d8fa6f72256220447c7f99d1b4b
-croot
-
 rm -rf vendor/evolution-priv
 
 # Refresh signing keys
