32dd1b2abebf5eed79f41deb7d364aa3a3256667
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-08-23 04:25:08 +0700

    Update vendorsetup.sh for lineage-priv changes
    Removed references to AxionFx and steam-audio, updated signing key removal and cloning to lineage-priv.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -50,17 +50,13 @@ git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages
 rm -rf packages/apps/TouchServices
 git clone https://github.com/ryznstk/packages_apps_TouchServices.git -b lineage-23.2 packages/apps/TouchServices
 
-rm -rf packages/apps/AxionFx
-rm -rf external/steam-audio
-rm -rf vendor/lineage-priv
-
 # Refresh signing keys
-if [ -d vendor/evolution-priv/keys ]; then
+if [ -d vendor/lineage-priv/keys ]; then
   echo "Removing existing signing keys..."
-  rm -rf vendor/evolution-priv/keys
+  rm -rf vendor/lineage-priv/keys
 fi
 echo "Cloningfresh signing keys..."
-git clone https://github.com/ryznstk/keys.git vendor/evolution-priv/keys
+#git clone https://github.com/ryznstk/keys.git vendor/evolution-priv/keys
 
 # Always back to root at the end
 if command -v croot &>/dev/null; then
